2e541d1cda2b424cb0add1bee6d8f27ee59bf40b
Andrew Bello <92801912+Andrew-Bello@users.noreply.github.com>
2024-11-05 15:17:33 +0000

    My version of the project

---
+++ 02_Progress_Steps/2.1_progress_steps/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const rects = document.querySelectorAll('.rect');
+
+let currentActive = 1;
+
+function update() {
+  rects.forEach((rect, idx, a) => {
+    if (idx < currentActive) {
+      rect.classList.add('done');
+    } else {
+      rect.classList.remove('done');
+    }
+
+    rect.classList.toggle('active', idx === currentActive - 1);
+
+    prev.disabled = currentActive === 1;
+    next.disabled = currentActive === rects.length + 1;
+  });
+}
+
+prev.addEventListener('click', () => {
+  currentActive--;
+  if (currentActive < 1) currentActive = 1;
+  update();
+});
+
+next.addEventListener('click', () => {
+  currentActive++;
+  if (currentActive > rects.length) currentActive = rects.length + 1;
+  update();
+});
